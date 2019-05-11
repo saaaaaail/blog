35281b43d20f498f5a6a5b62aa05ca7b40d93430
saaaaaail <649014081@qq.com>
2019-05-11 21:18:47 +0800

    redis config

--- blog-common/src/main/java/com/sail/blog/config/RedisConfig.java
+++ blog-common/src/main/java/com/sail/blog/config/RedisConfig.java
@@ -1,5 +1,13 @@
 package com.sail.blog.config;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
 /**
  * @program: blog
  * @description: redis配置
@@ -7,6 +15,43 @@ package com.sail.blog.config;
  * @create: 2019/05/10 19:46
  */
 
-public class RedisConfig {
+@Log4j2
+@Configuration
+@EnableCaching//启用缓存
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.database}")
+    private int database;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    @Value("${spring.redis.pool.max-idle}")
+    private int maxIdle;
+
+    @Value("${spring.redis.pool.max-wait}")
+    private long maxWaitMillis;
+
+    @Value("${spring.redis.pool.min-idle}")
+    private int minIdle;
+
+    @Value("${spring.redis.pool.max-active}")
+    private int maxActive;
+
+    /**
+     * 注入 RedisConnectionFactory
+     */
+    @Autowired
+    JedisConnectionFactory redisConnectionFactory;
+
 
 }
