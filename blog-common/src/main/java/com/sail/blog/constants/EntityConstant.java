a25dff1d8edd83774d4fd9405766e23fc1256363
saaaaaail <649014081@qq.com>
2019-07-18 20:24:30 +0800

    类型常量

---
+++ blog-common/src/main/java/com/sail/blog/constants/EntityConstant.java
@@ -0,0 +1,13 @@
+package com.sail.blog.constants;
+
+/**
+ * @program: blog
+ * @description:
+ * @author: yangfan
+ * @create: 2019/07/12 18:37
+ */
+
+public class EntityConstant {
+    public static final Integer ENTITY_TYPE_QUESTION = 0;
+    public static final Integer ENTITY_TYPE_COMMENT = 1;
+}
